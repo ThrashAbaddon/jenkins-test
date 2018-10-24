6742e4e7edb99f849098800026a8c90a0b944ae6
Daniel <straca369@gmail.com>
2018-10-24 21:44:47 +0200

    Abstract engine in a car

--- src/models/car.py
+++ src/models/car.py
@@ -1,10 +1,10 @@
 class Car(object):
-
-    def __init__(self, doors, horsepower):
+    def __init__(self, doors, horsepower, engine):
+        assert isinstance(engine, Engine), 'Has to be Engine type.'
         self._doors = doors
         self._horsepower = horsepower
+        self._engine = engine
         super(Car, self).__init__()
-
 
     @property
     def doors(self):
@@ -12,4 +12,23 @@ class Car(object):
 
     @property
     def horsepower(self):
+        return self.engine.horsepower
+
+    @property
+    def engine(self):
+        return self._engine
+
+
+class Engine(object):
+    def __init__(self, horsepower, consumption):
+        self._horsepower = horsepower
+        self._consumption = consumption
+        super(Engine, self).__init__()
+
+    @property
+    def horsepower(self):
         return self._horsepower
+
+    @property
+    def consumption(self):
+        return self._consumption
